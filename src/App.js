db19367421628ebec090681e1cba8520b87db26d
Marissa <marissagvozdenovich@gmail.com>
2021-03-04 15:25:04 -0500

    begin chat app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { useState } from 'react';
+import useSocket from 'use-socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [id, setId] = useState('');
+	const [nameInput, setNameInput] = useState('');
+	const [room, setRoom] = useState('');
+	const [socket] = useSocket('https://open-chat-naostsaecf.now.sh');
+	socket.connect();
+	console.log(socket);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!nameInput) {
+			return alert('Name cannot be empty');
+		}
+		setId('');
+		socket.emit('join', /*name*/ room);
+	};
+	return id !== '' ? (
+		<div>Hello</div>
+	) : (
+		<div style={{ textAlign: 'center', margin: '30vh auto', width: '70%' }}>
+			<form onSubmit={(event) => handleSubmit(event)}>
+				<input
+					id='name'
+					onChange={(e) => setNameInput(e.target.value.trim())}
+					required
+					placeholder='What is your name?'
+				/>
+				<br />
+				<input
+					id='room'
+					onChange={(e) => setRoom(e.target.value.trim())}
+					placeholder='What is your room'
+				/>
+				<br />
+				<button type='submit'>Submit</button>
+			</form>
+		</div>
+	);
+};
 
 export default App;
